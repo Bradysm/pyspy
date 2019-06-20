7b0713e85d5ebb40de3217dd2f944299d434c32e
bradysm <bradysm@vt.edu>
2019-06-20 00:34:52 -0400

    directory management and .sh created

---
+++ pyspy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# the purpose of this script is to do the setup and begin running the keylogger for you
+# this script will take your pyspy server email and password. It will then add the 
+# pair to the keyring for authentication use.
+
+# Brady Murphy
+# June 15, 2019
+
+
+# get the email and password from the command line
+	# kick them if needed
+
+# install the python modules if needed
+
+
+# add email and password to keyring
+python3 
